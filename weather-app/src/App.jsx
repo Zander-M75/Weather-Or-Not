f453829e5dd03af587d61d91af5db84d4dbec4e9
Zander <94405288+Zmarenberg@users.noreply.github.com>
2023-12-04 16:09:34 -0500

    axios installed

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import './App.css'
 import Home from './Pages/Home'
+import Hero from './Components/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Home />
+      <Hero />
     </>
   )
 }
